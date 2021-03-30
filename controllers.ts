8383a769f4e1cdb3c41a2eb6db5b5419e118dbad
Billy Kirk <kirk.william.c@gmail.com>
2021-03-29 19:26:36 -0500

    Making error handling more concise

--- controllers.ts
+++ controllers.ts
@@ -2,13 +2,10 @@ import { Context, helpers } from "https://deno.land/x/oak/mod.ts";
 import { Database } from "https://deno.land/x/aloedb/mod.ts";
 import { Todo } from "./db/todos/todo.ts";
 
-const noResultsFoundObject = {
-  error: "no results found",
-};
-
 export async function getAllTodos(context: Context) {
   const db = new Database<Todo>("./db/todos/todos.json");
   const results = await db.findMany();
+  context.response.status = results ? 200 : 404;
   context.response.body = results || {};
 }
 
@@ -17,11 +14,7 @@ export async function getTodoById(context: Context) {
   if (params && params.id) {
     const db = new Database<Todo>("./db/todos/todos.json");
     const results = await db.findOne({ id: parseInt(params.id) });
-    if (results) {
-      context.response.body = results;
-    } else {
-      context.response.status = 404;
-      context.response.body = noResultsFoundObject;
-    }
+    context.response.status = results ? 200 : 404;
+    context.response.body = results || {};
   }
 }
